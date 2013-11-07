5fdd54768486eb02d7fc5c93d7c7c2fe72ee133e
Aleksandr <andreev-a@open.ru>
2013-11-07 12:19:05 +0400

    Remove URI

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,14 +1,14 @@
-require 'uri'
+#require 'uri'
 require 'net/http'
 
 module Ipgeobase
-  URL = 'http://ipgeobase.ru:7020/geo'
+  #URL = 'http://ipgeobase.ru:7020/geo'
   autoload 'IpMetaData', 'ipgeobase/ip_meta_data'
 
   def self.lookup(ip)
-    uri = URI.parse(URL)
-    uri.query = URI.encode_www_form :ip => ip
+    #uri = URI.parse(URL)
+    #uri.query = URI.encode_www_form :ip => ip
 
-    IpMetaData.parse(Net::HTTP.get(uri))
+    IpMetaData.parse(Net::HTTP.get('http://ipgeobase.ru:7020/geo?ip=' + ip))
   end
 end
